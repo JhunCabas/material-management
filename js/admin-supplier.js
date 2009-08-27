d1a3ccdf7bb89b673c6c8d14ce6f5b780e45d6a3
shernshiou <shernshiou@7c670fb8-912e-11de-9fc0-fbe434d33be3>
2009-08-27 09:05:26 +0000

    Updated supplier function

--- js/admin-supplier.js
+++ js/admin-supplier.js
@@ -26,10 +26,12 @@ $(function (){
 		 $(this).parent().parent().addClass("hideFirst");
 	 	 var key = $(this).parent().parent().parent().find("#sId").text();
 	 	 var name = $(this).parent().parent().parent().find("input#sName").val();
+		 var address = $(this).parent().parent().parent().find("input#sAddress").val();
+		 var person = $(this).parent().parent().parent().find("input#sPerson").val();
 	 	 var contact = $(this).parent().parent().parent().find("input#sContact").val();
 	 	 var info = $(this).parent().parent().parent().find("input#sInfo").val();
 		 $(this).parent().parent().parent().addClass("edit"+key);
-		 $.post("parser/Supplier.php",{type: "edit", key: key, name: name, contact: contact, info:info},
+		 $.post("parser/Supplier.php",{type: "edit", key: key, name: name, address: address, contact_person: person, contact: contact, info:info},
 		 function (data){
 		 	if(data != "")
 			 {
@@ -44,8 +46,33 @@ $(function (){
 				 $(".edit"+key).find("#sName").text(name);
 				 $(".edit"+key).find("#sContact").text(contact);
 				 $(".edit"+key).find("#sInfo").text(info);
+				 $(".edit"+key).find("#sAddress").text(address);
+				 $(".edit"+key).find("#sPerson").text(person);
 				 $(".edit"+key).removeClass("edit"+key);
 			 }
 		 });
+	 });
+	$("li#add").click(function (){
+		 var name = $("#supName").val();
+		 var address = $("#supAddress").val();
+		 var person = $("#supPerson").val();
+		 var contact = $("#supContact").val();
+		 var info = $("#supInfo").val();
+		 if(confirm("Continue?"))
+			$.post("parser/Supplier.php",{type: "add", name: name, address: address, contact: contact, contact_person: person, info: info},function (data){
+		 		if(data != "")
+				 {
+				 	 $("#dialogBox").html(data);
+					 $("#dialogBox").dialog('option', 'title', 'Error');
+					 $("#dialogBox").dialog('open');
+				 }
+				 else{
+				 	$("#dialogBox").html("<span class=\"ui-icon ui-icon-info\" style=\"float: left; margin-right: 0.3em;\"/>Added");
+					$("#dialogBox").dialog('option', 'title', 'Success');
+					$("#dialogBox").dialog('open');
+					var prependedText = "<tr><td>"+name+"</td><td>"+address+"</td><td>"+person+"</td><td>"+contact+"</td><td>"+info+"</td></tr>";
+					$("#newItem").before(prependedText);
+				 }
+			});
 	 });
 });
--- parser/Supplier.php
+++ parser/Supplier.php
@@ -12,6 +12,15 @@
 			} catch (fExpectedException $e) {
 				echo $e->printMessage();
 			}
+		}else if($_POST['type'] == "add")
+		{
+			try {
+				$supplier = new Supplier();
+				$supplier->populate();
+				$supplier->store();
+			} catch (fExpectedException $e) {
+				echo $e->printMessage();
+			}
 		}
 	}
 ?>
