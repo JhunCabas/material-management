b5cc38e9b0bd596ee8b340e6162de9bb24f4c130
shernshiou <shernshiou@7c670fb8-912e-11de-9fc0-fbe434d33be3>
2009-08-27 09:05:26 +0000

    Updated supplier function

--- trunk/admin-supplier.php
+++ trunk/admin-supplier.php
@@ -11,7 +11,7 @@ $tmpl->place('menu');
 	<h2>Administration</h2><h3>Supplier</h3>
 	<table>
 		<thead>
-			<tr><th>Name</th><th>Contact</th><th>Information</th><th>Icon</th></tr>
+			<tr><th>Name</th><th width="400">Address</th><th>Contact Person</th><th>Contact</th><th>Information</th><th>Icon</th></tr>
 		</thead>
 		<tbody>
 			<?php
@@ -19,9 +19,11 @@ $tmpl->place('menu');
 					$suppliers = Supplier::findAll();
 					foreach($suppliers as $supplier)
 					{
-						printf("<tr class=\"supplierRow\"><td id=\"sId\" class=\"hideFirst\">%s</td><td id=\"sName\" class=\"varInput\">%s</td><td id=\"sContact\" class=\"varInput\">%s</td><td id=\"sInfo\" class=\"varInput\">%s</td>"
+						printf("<tr class=\"supplierRow\"><td id=\"sId\" class=\"hideFirst\">%s</td><td id=\"sName\" class=\"varInput\">%s</td><td id=\"sAddress\" class=\"varInput\">%s</td><td id=\"sPerson\" class=\"varInput\">%s</td><td id=\"sContact\" class=\"varInput\">%s</td><td id=\"sInfo\" class=\"varInput\">%s</td>"
 							,$supplier->prepareId()
 							,$supplier->prepareName()
+							,$supplier->prepareAddress()
+							,$supplier->prepareContactPerson()
 							,$supplier->prepareContact()
 							,$supplier->prepareInfo());
 						printf("<td id=\"iconCell\" class=\"hideFirst\"><ul id=\"icons\" class=\"ui-widget ui-helper-clearfix\">
@@ -35,6 +37,8 @@ $tmpl->place('menu');
 			?>
 			<tr id="newItem">
 				<td><input id="supName" value="Input Name"></input></td>
+				<td><input id="supAddress" value="Input Address"></input></td>
+				<td><input id="supPerson" value="Input Contact Person"></input></td>
 				<td><input id="supContact" value="Input Contact"></input></td>
 				<td><input id="supInfo" value="Input Information"></input></td>
 				<td id="iconCell">
