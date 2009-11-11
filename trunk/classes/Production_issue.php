0c99c2a474c4854944dd171860b90328e9279163
shernshiou <shernshiou@7c670fb8-912e-11de-9fc0-fbe434d33be3>
2009-11-11 07:30:47 +0000

    Move to trunk

--- trunk/classes/Production_issue.php
+++ trunk/classes/Production_issue.php
@@ -20,7 +20,7 @@
 	static function findByBranch($branch,$doctype)
 	{
 		return fRecordSet::buildFromSQL('Production_issue',
-				"SELECT production_issues.* FROM production_issues, (SELECT * FROM users WHERE branch_id = '$branch') AS tbl WHERE production_issues.issuer = tbl.username AND production_issues.doc_type = '$doctype'",
+				"SELECT production_issues.* FROM production_issues, (SELECT * FROM users WHERE branch_id = '$branch') AS tbl WHERE production_issues.issuer = tbl.username AND production_issues.doc_type = '$doctype' AND YEAR( production_issues.doc_date ) = YEAR( CURDATE( ) ) AND MONTH( production_issues.doc_date ) = MONTH( CURDATE( ))",
 				"SELECT count(*) FROM production_issues"
 			);
 	}
