aaf17a38d5726f3cfae79642bb02405b8944e7f5
shernshiou <shernshiou@7c670fb8-912e-11de-9fc0-fbe434d33be3>
2009-12-13 15:27:21 +0000

    Fix running number in PI and MTR

--- classes/Good_receipt_note.php
+++ classes/Good_receipt_note.php
@@ -16,6 +16,15 @@
 			array('doc_date' => 'desc'),
 			$limit);    
     }
+
+	static function findCurrentMonth($branch)
+	{		
+		return fRecordSet::buildFromSQL('Good_receipt_note',
+				"SELECT good_receipt_notes.* FROM good_receipt_notes WHERE good_receipt_notes.branch_id = '$branch' 
+				AND YEAR( good_receipt_notes.doc_date ) = YEAR( CURDATE( )) AND MONTH( good_receipt_notes.doc_date ) = MONTH( CURDATE( ))",
+				"SELECT count(*) FROM good_receipt_notes"
+			);			
+	}
  }
 
 ?>
