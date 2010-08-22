41e31c78b247b48da11b125cf1ddef010198b266
bmahendran <bmahendran@7c670fb8-912e-11de-9fc0-fbe434d33be3>
2010-08-22 07:09:02 +0000

    added link to PZila

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,4 +1,6 @@
-<div id="bottomline" class="span-24 last"><?php echo $this->prepare('title')." ".$this->prepare('version'); ?></div>
+<div id="bottomline" class="span-24 last"><?php echo $this->prepare('title')." ".$this->prepare('version');?>
+<?php echo "- PocketZila IT System"; ?></div>
+
 </div>
 <div id="dialogBox"></div>
 </body>
