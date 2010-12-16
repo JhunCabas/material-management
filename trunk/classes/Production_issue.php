c2f83d4da5e48118f209a85f6d2164c3385e2d91
shernshiou <shernshiou@7c670fb8-912e-11de-9fc0-fbe434d33be3>
2010-12-16 18:53:44 +0000

    All pending and 10 completed in list-pi.php

--- trunk/classes/Production_issue.php
+++ trunk/classes/Production_issue.php
@@ -10,7 +10,7 @@
      * 
      * @return fRecordSet  An object containing all entries
      */
-	static function findAll($limit=null)
+	public static function findAll($limit=null)
 	{
 		return fRecordSet::build('Production_issue',
 			array('status!' => 'cancelled'),
@@ -18,7 +18,15 @@
 			$limit); 
 	}
 	
-	static function findByBranch($branch,$doctype)
+	public static function findByStatus($status, $limit=null)
+	{
+		return fRecordSet::build('Production_issue',
+			array('status=' => $status),
+			array('doc_date' => 'desc'),
+			$limit);
+	}
+	
+	public static function findByBranch($branch,$doctype)
 	{
 		return fRecordSet::buildFromSQL('Production_issue',
 				"SELECT production_issues.* FROM production_issues, (SELECT * FROM users WHERE branch_id = '$branch') AS tbl WHERE production_issues.issuer = tbl.username AND production_issues.doc_type = '$doctype' AND YEAR( production_issues.doc_date ) = YEAR( CURDATE( ) ) AND MONTH( production_issues.doc_date ) = MONTH( CURDATE( ))",
