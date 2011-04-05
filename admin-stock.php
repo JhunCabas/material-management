420a6a1ca88320ca376b67208c393e649d42c955
shernshiou <shernshiou@gmail.com>
2011-04-05 08:36:12 +0000

    Modal plugin + UI changes

--- admin-stock.php
+++ admin-stock.php
@@ -11,9 +11,9 @@ $tmpl->place('header');
 $tmpl->place('menu');
 ?>
 <div id="content" class="span-24 last">
-	<?php $tmpl->place('menuInventory'); ?>
+	<?php $tmpl->place('menuAdmin'); ?>
 	<div id="main" class="span-24 last">
-		<h2>Stock</h2>
+		<h2>Administration</h2><h3>Stock</h3>
 		<br />
 		<form action = "admin-stock.php" method = "get">
 			<label for="inv_item">Choose Inventory Item: </label>
@@ -28,12 +28,12 @@ $tmpl->place('menu');
 			try{
 				$inv_stocks = Inv_stock::findByItem($_GET['item']);
 				$item = new Inv_item($_GET['item']);
-				printf("<br /><h3>%s - %s</h3><table><thead><tr><th>Branch</th><th>Quantity</th></tr></thead><tbody>"
+				printf("<br /><h3>%s - %s</h3><table><thead><tr><th>Branch</th><th>Quantity</th><th width=\"100\">Update</th></tr></thead><tbody>"
 					,$item->prepareId(),$item->prepareDescription());
 				foreach($inv_stocks as $inv_stock)
 				{
 					$branch = new Branch($inv_stock->getBranchId());
-						printf("<tr><td>%s</td><td>%s</td></tr>",$branch->prepareName()." [".$branch->prepareId()."]",$inv_stock->prepareQuantity()." ".$item->prepareUnitOfMeasure());
+						printf("<tr><td>%s</td><td><input type=\"text\" value=\"%s\" /> %s</td><td><input type=\"button\" value=\"Update\"/></td></tr>",$branch->prepareName()." [".$branch->prepareId()."]",$inv_stock->prepareQuantity(),$item->prepareUnitOfMeasure());
 				}
 				echo "</tbody></table>";
 			} catch (fExpectedException $e) {
